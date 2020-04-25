499ce7c70676fcfba9eebec07b7d865c6a849a7d
Maxim Palchikov <palchmx@gmail.com>
2020-04-25 06:37:50 +0300

    render items as is

--- src/components/TableWithGroups.vue
+++ src/components/TableWithGroups.vue
@@ -1,9 +1,25 @@
 <template>
-  <h1>Table With Groups</h1>
+  <div>
+    <h1>Table With Groups</h1>
+    <table border="1">
+      <thead>
+        <tr>
+          <th>Groups: {{ groups.join("/") }}</th>
+          <th>City</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in items" :key="index">
+          <td></td>
+          <td>{{ item.city }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
 export default {
-  properties: ["items", "groups"]
+  props: ["items", "groups"]
 };
 </script>
